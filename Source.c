e018c0de6ae2aaab66ebdab6078fbc7942b9e99f
Ctibor Kovalčík <71821764+steboee@users.noreply.github.com>
2020-11-01 14:34:42 +0100

    UPDATE 1.11 ráno
    rodne cislo prave riesim

--- Source.c
+++ Source.c
@@ -1,19 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 function_v() {
 	FILE* file = fopen("pacienti.txt", "r");
 	char buff[100];
-	int pocet_riadkov =0, pacienti=0;
+	int pocet_riadkov = 0, pacienti = 0;
 	
 	for (int i = 1; fgets(buff, sizeof(buff), file) != NULL; i++) { // zistovanie poctu riadkov v zaznamoch
 		pocet_riadkov++;
 	}
-	fseek(file, 1,SEEK_SET);
+	fseek(file, 1,SEEK_SET);						 // nastavenie sa v subore na zaciatok.
+	
 	int x;
-	x = pocet_riadkov; //x = lokalna premenna pre nasledujuci for aby nemenula premennu pocet_riadkov
-	for (x; x > 0; x = x - 7) { // zistenie poctu pacientov
+	x = pocet_riadkov;								//x = lokalna premenna pre nasledujuci for aby nsa zachovala premenná "pocet_riadkov"
+	
+	for (x; x > 0; x = x - 7) {						// zistenie poctu pacientov
 		pacienti++;
 	}
 	
@@ -27,66 +29,65 @@ function_v() {
 	
 	for (int i = 0; i < pacienti; i++) {
 		
-		pole_meno[i] = calloc(50,sizeof(char));
-		fgets(pole_meno[i], 50 - 1, file);
-		printf("Meno priiezvisko: %s", pole_meno[i]);
-		pole_rodnecislo[i] =calloc(50,sizeof(char*));
+		pole_meno[i] = calloc(100,sizeof(char));			// 1. vytvorenie pola v poli na pozicii i 
+		fgets(pole_meno[i], 100, file);				// 2. Priradenie stringu
+		if (strlen(pole_meno[i]) > 50) {
+			printf("ERROR : meno nesplna poziadavky\n");
+		}
+		else {
+			printf("Meno priezvisko: %s", pole_meno[i]);	// 3. vypisanie stringu     
+		}
+		  
+
+		pole_rodnecislo[i] =calloc(10,sizeof(char));
 		fgets(pole_rodnecislo[i], 50 - 1, file);
-		printf("Rodne cislo: %s", pole_rodnecislo[i]);
+		//mlady narodený od roku 2000
+		if ((pole_rodnecislo[i])[0] == '0'){				// 1. ak rodne cislo nejakeho pacienta zacina 0
+			(pole_rodnecislo[i])[0] = '1';					// 2. prevediem 0 na 1 aby som mohol previest tento string na int
+			int rod = atoi(pole_rodnecislo[i]);
+			(pole_rodnecislo[i])[0] = '0';												// 3. string ---> int
+			rod = rod - 1000000000;							// 4. rodne cislo je 10 miestne , v (2.) som akokeby pripocital  1 000 000 000
+			if (rod % 11 != 0) {								// tak teraz odcitam 1 000 000 000 
+				printf("ERROR: neplatne rodne cislo mlady!\n");
+			}
+			else {
+				printf("Rodne cislo: %s", pole_rodnecislo[i]);
+			}
+		}
+		// narodeny pred rokom 2000
+		else {
+			int rod = atoi(pole_rodnecislo[i]);
+			printf("%d\n", rod);
+			if (rod % 11 != 0) {
+				printf("ERROR: nespravne rodne cislo stary \n!");
+			}
+			else {
+				printf("Rodne cislo: %s", pole_rodnecislo[i]);
+			}
+		}
+		
+		
+
 		pole_diagnoza[i] = calloc(50,sizeof(char*));
 		fgets(pole_diagnoza[i], 50 - 1, file);
 		printf("Diagnoza: %s", pole_diagnoza[i]);
+
 		pole_vysetrenie[i] = calloc(50,sizeof(char*));
 		fgets(pole_vysetrenie[i], 50 - 1, file);
 		printf("Vysetrenie: %s", pole_vysetrenie[i]);
+
 		pole_vysledok[i] = calloc(50, sizeof(char*));
 		fgets(pole_vysledok[i], 50 - 1, file);
 		printf("Vysledok: %s", pole_vysledok[i]);
+
 		pole_datum[i] = calloc(50,sizeof(char*));
 		fgets(pole_datum[i], 50 - 1, file);
 		printf("Datum: %s", pole_datum[i]);
+
 		puts("");
 		fgets(buff, 50, file);
 	
 	}
-	printf("POELEEEE %s", pole_meno[2]);
-	/*int pocitadlo = 0;
-	for (int i = 0; i < pocet_riadkov;i++) {
-		if (i == 7 * pocitadlo + 1) {
-			fgets(pole_meno[i], 50 - 1, file);
-			printf("Meno priiezvisko: %s", pole_meno[i]);
-		}
-		if (i == 7 * pocitadlo + 2) {
-			fgets(pole_rodnecislo[i], 50 - 1, file);
-			printf("Rodne cislo: %s", pole_rodnecislo[i]);
-		}
-		if (i == 7 * pocitadlo + 3) {
-			fgets(pole_diagnoza[i], 50 - 1, file);
-			printf("Diagnoza: %s", pole_diagnoza[i]);
-		}
-		if (i == 7 * pocitadlo + 4) {
-			fgets(pole_vysetrenie[i], 50 - 1, file);
-			printf("Vysetrenie: %s", pole_vysetrenie[i]);
-		}
-		if (i == 7 * pocitadlo + 5) {
-			fgets(pole_vysledok[i], 50-1, file);
-			printf("Vysledok: %s", pole_vysledok[i]);
-		}
-		if (i == 7 * pocitadlo + 6) {
-			fgets(pole_datum[i], 50-1, file);
-			printf("Datum: %s", pole_datum[i]);
-		}
-		if (i == 7 + 7*pocitadlo) {
-			printf("\n");
-			fgets(buff, sizeof(buff), file);
-			pocitadlo++;
-		}
-		
-
-	}
-	*/
-	
-
 }
 
 function_o() {
